52a76a697bdf95660297a6dbba8ddb65efb63d69
TooEnsureDevelopment <iosdevenv@gmail.com>
2020-08-05 09:43:37 -0700

    Test: Adding my dev tool

---
+++ Solo_DevStack/Logo.ps1
@@ -0,0 +1,8 @@
+# Copyright 2020
+Write-Host 'AUTOMATING FOR YOU' -ForegroundColor Green
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+Write-Host "                                                     "
+Write-Host '              Too-Ensure Solo Experaince'             -ForegroundColor Green
+Write-Host '               ~~~~ Template Sweety ~~~~'             -ForegroundColor Yellow
+Write-Host "                                                     "
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
